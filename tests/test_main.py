b79c8217b708d66ab05359b5a3683859e8a97b79
Sean Doyle <seandoyle@kubrickgroup.com>
2025-11-04 17:00:36 +0000

    working on refactoring unittests

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,33 +12,49 @@ GET_TASKS_HEADER = """Tasks:
 
 GET_TASKS_TAIL = "-" * 50 + "\n"
 
-def assemble_get_tasks_str(tasks:str=""):
-    return "{}{}{}".format(GET_TASKS_HEADER, tasks, GET_TASKS_TAIL)
+def make_list_generic_test(
+        capsys,
+        monkeypatch,
+        inputs:list[str],
+        lst:list[str],
+        expected_lst:list[str],
+        expected_out:str,
+        expected_input_args:list[str]
+    ):
+    next_input = iter(inputs).__next__
+    input_args = []
+    def input_patch(input_arg):
+        input_args.append(input_arg)
+        return next_input()
+    monkeypatch.setattr("builtins.input", input_patch)
+    res = makelist(lst)
+    assert res == 1
+    assert lst == expected_lst
+    captured = capsys.readouterr()
+    assert captured.out == expected_out
+    assert input_args == expected_input_args
 
 
 def test_add_task(capsys, monkeypatch):
-    next_input = iter(["1", "new task"]).__next__
-    monkeypatch.setattr("builtins.input", lambda _ : next_input())
-    lst = []
-    res = makelist(lst)
-    assert res == 1
-    assert lst == ["new task"]
-    captured = capsys.readouterr()
-    expected = EXPECTED_TASK_HEADER + "-" * 50 + "\n"
-    assert captured.out == expected
 
-    next_input = iter(["1", "new task"]).__next__
-    monkeypatch.setattr("builtins.input", lambda _ : next_input())
-    lst = ["first", "second", "third"]
-    res = makelist(lst)
-    assert res == 1
-    assert lst == ["first", "second", "third", "new task"]
-    captured = capsys.readouterr()
-    expected = EXPECTED_TASK_HEADER + "-" * 50 + "\n"
-    assert captured.out == expected
+    def add_task_helper(initial_task_list, expected_task_list):
+        make_list_generic_test(
+            capsys,
+            monkeypatch,
+            ["1", "new task"],
+            initial_task_list,
+            expected_task_list,
+            
+            EXPECTED_TASK_HEADER + "-" * 50 + "\n",
+            ["Enter your choice: ", "Enter task: "]
+        )
+    add_task_helper([],["new task"],)
+    add_task_helper(["first", "second", "third"], ["first", "second", "third", "new task"],)
 
 
 def test_get_tasks(capsys, monkeypatch):
+    def assemble_get_tasks_str(tasks:str=""):
+        return "{}{}{}".format(GET_TASKS_HEADER, tasks, GET_TASKS_TAIL)
     monkeypatch.setattr("builtins.input", lambda _ : "2")
     res = makelist([])
     assert res == 2
